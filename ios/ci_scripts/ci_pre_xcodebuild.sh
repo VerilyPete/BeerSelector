6d573613de3d641526031adbe02b6e6d6a5e2f7b
Test User <test@example.com>
2025-04-15 11:26:33 -0500

    Adding node install to prebuild script

--- ios/ci_scripts/ci_pre_xcodebuild.sh
+++ ios/ci_scripts/ci_pre_xcodebuild.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+set -e
+
+echo "Setting up Node.js environment..."
+# Install Node.js using the built-in Ruby
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm install 18 # or whatever version you need
+
+echo "Current directory: $(pwd)"
 cd "$(dirname "$0")/../"
+echo "Changed to iOS directory: $(pwd)"
+
+echo "Node version: $(node --version)"
 echo "Installing CocoaPods..."
 pod install --repo-update
